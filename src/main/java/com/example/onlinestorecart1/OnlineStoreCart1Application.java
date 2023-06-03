081a6ff6c8ed5c8b28de4d81cac0e1f09eacb453
Сергей <mak.sergei@ngs.ru>
2023-06-04 00:07:27 +0700

    one_commit

--- src/main/java/com/example/onlinestorecart1/OnlineStoreCart1Application.java
+++ src/main/java/com/example/onlinestorecart1/OnlineStoreCart1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineStoreCart1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineStoreCart1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreCart1Application.class, args);
+    }
 
 }
